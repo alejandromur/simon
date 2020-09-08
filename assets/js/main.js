1f51ea606c5773d901fec71b3e61e7315fa110d9
Alejandro Mur <alejandro.mur@boream.com>
2020-09-08 18:12:16 +0200

    Refactor Make the opacity of the shapes less brilliant Add temporal control buttons in the markup Add two main handlers: startGame and endGame Bind events Simplify computer and human turn tasks Update the function that handles each turn

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,111 +3,72 @@
   -----
   - Different difficult levels
   - Increase speed when the user completes each level
+	
+  DOING:
+	-----
+  - Modify resetGame() from location.reload() to restart app
 
+	
   WISHLIST:
   ---------
 
   BUG:
   ----
 */
+
 class Simon {
 	constructor(elem) {
-		this.game = document.querySelector(elem)
+		this.board = document.querySelector(elem)
+		this.startButton = document.querySelector('.js-start')
+		this.endButton = document.querySelector('.js-end')
 		this.colors = ['red', 'green', 'blue', 'yellow']
 		this.colorsLength = this.colors.length
-		this.round = 0
-		this.humanCounter = 0
+		this.gameIsRunning = null
 		this.turn = null
+		this.round = 1
 		this.pattern = []
-		this.humanAnswer = []
-		this.elements = {
-			red: this.game.querySelector('[data-color="red"]'),
-			blue: this.game.querySelector('[data-color="blue"]'),
-			yellow: this.game.querySelector('[data-color="yellow"]'),
-			green: this.game.querySelector('[data-color="green"]'),
-		}
-	}
-
-	init() {
-		// Get level selected
-		// Create this fn
-		this.game.addEventListener('click', event => this.captureEvent(event))
-		this.turn = 'computer'
-		this.game.setAttribute('data-turn', 'computer')
-		this.addRandomColor()
-	}
-
-	increaseRoundCounter() {
-		this.round++
-	}
-
-	toggleTurn() {
-		if (this.turn === 'computer') {
-			this.turn = 'human'
-			this.game.setAttribute('data-turn', 'human')
-		} else {
-			this.turn = 'computer'
-			this.game.setAttribute('data-turn', 'computer')
-		}
-		console.log(this.turn)
-	}
-
-	addRandomColor() {
-		let randomColor = Math.floor(Math.random() * this.colorsLength)
-		this.pattern = [...this.pattern, this.colors[randomColor]]
-		this.highlightPattern()
-	}
-
-	highlightPattern() {
-		const highlightColor = (color, ms) =>
-			new Promise((resolve, reject) => {
-				setTimeout(() => {
-					this.elements[color].classList.add('is-active')
-				}, ms)
-				setTimeout(() => {
-					resolve(this.elements[color].classList.remove('is-active'))
-				}, ms + 500)
-			})
-
-		async function asyncForEach(array, callback) {
-			for (let index = 0; index < array.length; index++) {
-				await callback(array[index], index, array)
-			}
-		}
-
-		const start = async () => {
-			await asyncForEach(this.pattern, async color => {
-				console.log(color)
-				await highlightColor(color, 1000)
-			})
-			console.log('Done')
-			this.nextTurn()
-		}
-
-		start()
-	}
-
-	nextTurn() {
-		this.increaseRoundCounter()
-		this.toggleTurn()
-
-		if (this.turn === 'computer') {
-			this.addRandomColor()
-		} else {
-			this.resetHumanAnswer()
-		}
-	}
-
-	resetHumanAnswer() {
-		this.humanAnswer = []
 		this.humanCounter = 0
+		this.humanAnswer = []
 	}
 
-	captureEvent(event) {
+	bindEvents() {
+		this.startButton.addEventListener('click', this.startGame.bind(this))
+		this.endButton.addEventListener('click', this.endGame.bind(this))
+	}
+
+	startGame() {
+		console.log('game has started', this)
+		this.startButton.setAttribute('disabled', '')
+		this.endButton.removeAttribute('disabled')
+		this.board.addEventListener('click', this.listener.bind(this))
+		this.gameIsRunning = true
+		this.computerTurn()
+	}
+
+	endGame() {
+		console.log('game has ended')
+		this.endButton.setAttribute('disabled', '')
+		this.startButton.removeAttribute('disabled')
+		this.board.removeAttribute('data-turn')
+		this.board.removeEventListener('click', this.listener)
+		this.gameIsRunning = false
+		this.resetGame()
+	}
+
+	resetGame() {
+		window.location.reload()
+		// this.turn = null
+		// this.round = 1
+		// this.pattern = []
+		// this.humanCounter = 0
+		// this.humanAnswer = []
+	}
+
+	listener(event) {
 		const target = event.target.getAttribute('data-color')
 
-		if (!target) {
-			return false
+		if (!target || !this.gameIsRunning) {
+			return
 		}
 
 		if (target !== this.pattern[this.humanCounter]) {
@@ -121,15 +82,82 @@ class Simon {
 		this.humanCounter++
 	}
 
-	endGame() {
-		alert('The Game has ended')
+	computerTurn() {
+		this.turn = 'computer'
+		this.board.setAttribute('data-turn', 'computer')
+		// console.log(`Round: ${this.round}`)
+
+		const addRandomColor = () => {
+			let randomColor = Math.floor(Math.random() * this.colorsLength)
+			this.pattern = [...this.pattern, this.colors[randomColor]]
+			highlightPattern()
+		}
+
+		const highlightPattern = () => {
+			const highlightColor = (color, ms) =>
+				new Promise((resolve, reject) => {
+					setTimeout(() => {
+						this.board
+							.querySelector(`[data-color="${color}"]`)
+							.classList.add('is-active')
+					}, ms)
+					setTimeout(() => {
+						resolve(
+							this.board
+								.querySelector(`[data-color="${color}"]`)
+								.classList.remove('is-active')
+						)
+					}, ms + 500)
+				})
+
+			async function asyncForEach(array, callback) {
+				for (let index = 0; index < array.length; index++) {
+					await callback(array[index], index, array)
+				}
+			}
+
+			const start = async () => {
+				await asyncForEach(this.pattern, async color => {
+					console.log(
+						`Color: ${color} - Pattern: ${this.pattern} - Round: ${this.round}`
+					)
+					await highlightColor(color, 1000)
+				})
+				this.nextTurn()
+			}
+
+			start()
+		}
+
+		addRandomColor()
+	}
+
+	humanTurn() {
+		this.turn = 'human'
+		this.board.setAttribute('data-turn', 'human')
+		this.humanAnswer = []
+		this.humanCounter = 0
+	}
+
+	increaseRound() {
+		this.round++
+	}
+
+	nextTurn() {
+		if (!this.gameIsRunning) {
+			return false
+		}
+
+		if (this.turn === 'computer') {
+			this.humanTurn()
+		} else {
+			this.computerTurn()
+		}
+
+		this.increaseRound()
 	}
 }
 
-const game = new Simon('.app')
-const startButton = document.querySelector('.js-start')
-
-startButton.addEventListener('click', e => {
-	startButton.setAttribute('hidden', true)
-	game.init()
+document.addEventListener('DOMContentLoaded', () => {
+	new Simon('.app').bindEvents()
 })
